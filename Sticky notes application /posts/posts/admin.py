bd138ecdfd0860c8f78b1323f72b593c1e2fe53e
Tselane2 <huaweikcg@gmail.com>
2026-05-14 10:10:57 +0200

    Delete Sticky notes application /posts/posts directory

--- Sticky notes application /posts/posts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')
-    search_fields = ('title', 'content')
-    readonly_fields = ('created_at', 'updated_at')
